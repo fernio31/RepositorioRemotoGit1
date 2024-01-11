d53f2a0f574f37d4f76bb78f149b38f9b68becf1
DAW - Mañana <DAW - Maana@DESKTOP-QPSV11H>
2024-01-11 13:19:01 +0100

    Segunda confirmacion

--- Proyecto1/src/Paquete1/Principal.java
+++ Proyecto1/src/Paquete1/Principal.java
@@ -3,5 +3,6 @@ package Paquete1;
 public class Principal {
 	public static void main (String[]args) {
 		System.out.println("Ejemplo de control de versiones Git");
+		System.out.println("Segundo ejemplo de versiones en Git");
 	}
 }
